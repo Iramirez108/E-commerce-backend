708d39b0f81219025b07377f14005755a56bc66d
Iramirez108 <ivan.ramirez10208@gmail.com>
2022-09-05 22:13:15 -0500

    update models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,23 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id',
-
+  foreignKey: 'category_id',
 });
+
 // Categories have many Products
-Category.haveMany(Product, {
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
 }),
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  through: ProductTag,
   foreignKey: 'product_id',
-  onDelete: 'CASCADE',
 }),
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_id',
+  foreignKey: 'tag_id',
+  through: ProductTag,
 }),
+
 module.exports = {
   Product,
   Category,
